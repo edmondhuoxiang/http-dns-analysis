bf97431268549c22e7ee189a81b31b6333def36b
Xiang Huo <huoxiang5659@gmail.com>
2014-01-23 16:09:01 -0600

    insert data from dns log into db

--- dns-part/py/dnslog2db.py
+++ dns-part/py/dnslog2db.py
@@ -30,6 +30,37 @@ except pg.DatabaseError, e:
     Log.error(e.pgerror)
     sys.exit(1)
 
+def asDigitBinary(source):
+    return '{0:08b}'.format(source)
+
+def IsBeginWithPrefix(ip, prefix):
+    num = int(prefix.split('/')[1])
+    prefixStrArr = prefix.split('/')[0].split('.')
+    ipStrArr = ip.split('.')
+    i = num
+    j = 0
+    while(i>0)
+        str_1 = asDigitBinary(int(ipStrArr[j]))
+        str_2 = asDigitBinary(int(prefixStrArr[j]))
+        count=0
+        if i<8:
+            count = i
+        else:
+            count = 8
+        for k in range(0, count):
+            if(str_1[k] != str_2[k]):
+                return False
+        j = j+1
+        i = i-8
+    return True
+
+def isInPrefix(ip, prefixArr):
+    for prefix in prefixArr:
+        res = isBeginWithPrefix(ip, prefix)
+        if res == True:
+            return True
+    return False
+
 def log2db(lfile, tname):
     records = []
     ctr = 0
@@ -81,7 +112,7 @@ def log2db(lfile, tname):
             pass
         if ctr % 10000 == 0:
             if ctr % 50000 == 0:
-                print '......'
+                print '%d......' % ctr
             args = ','.join(cur.mogrify("(%s,%s,%s,%s,%s,%s,%s)",r) for r in records)
             try:
                 cur.execute('INSERT INTO %s %s VALUES %s' %(tname, tcolumns, args))
