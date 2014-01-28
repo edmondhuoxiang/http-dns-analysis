bff3a29361fddad1f07b0b61f3bfff5a3b031cf4
Xiang Huo <huoxiang5659@gmail.com>
2014-01-28 01:30:52 -0600

    change log file name

--- dns-part/py/estimate_query_rate.py
+++ dns-part/py/estimate_query_rate.py
@@ -24,7 +24,7 @@ tcolumns = '(ts, orig_h, resp_h, host, uri, referrer, method, user_agent, status
 try:
     con = pg.connect(database='tds', user='tds', host='localhost', password='9bBJPLr9')
     con.autocommit = True
-    cur = con.cursor()
+    cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
 except pg.DatabaseError, e:
     Log.error(e.pgerror)
     exit(1)
@@ -44,12 +44,112 @@ def getDomains(tname):
         results.append(str(domain)[2:-3])
     return results 
 
+MIN_SERIES_SIZE = 10
+
+class Record:
+    def __init__(self, tname, query):
+        global cur
+        self.resolvers = []
+        try:
+            cur.execute('SELECT DISTINCT orig_h FROM %s WHERE query = %s;' %(tname, query));
+        except pg.DatabaseError, e:
+            Log.error('%s : %s' %(tname, e))
+            exit(1)
+        while True:
+            resolver = cur.fetchone()
+            if resolver == None:
+                break
+            self.resolvers.append(str(resolver)[2:-3])
+        self.series = []
+        for i in range(0, len(self.resolvers)):
+            self.series.append([])
+
+
+        try:
+            cur.execute('SELECT * FROM %s where query = %s;' %(tname, query))
+        except pg.DatabaseError, e:
+            Log.error('%s : %s' %(tname, e))
+            exit(1)
+        self.domain = query
+        self.max_ttl = 0
+
+        while True:
+            record = cur.fetchone()
+            if record == None:
+                break
+            ts = record["ts"]
+            ttls = record["ttls"]
+            orig = record["orig_h"]
+
+            for i in range(0, len(self.resolver)):
+                if orig == self.resolver[i]:
+                    for ttl in ttls:
+                        self.series[i].append([ts, ttl])
+                        if ttl > self.max_ttl:
+                            self.max_ttl = ttl
+        self.series.sort(key=itemgetter(0))
+
+    def estimate_rate(self):
+        result = []
+        for j in range(0, len(self.resolver)):
+            if len(self.series[j]) < MIN_SERIES_SIZE:
+                result.append(-1)
+                continue
+
+            estimate = 0
+            count = 0
+            flag = False
+            for i in range(0, len(self.series[j])-1):
+                ts_1 = int(self.series[j][i+1][0])
+                ts_0 = int(self.series[j][i][0])
+                ttl = int(self.series[j][i][1])
+
+                delta = ts_1 - (ts_0 + ttl)
+
+                if delta < self.max_ttl:
+                    delta_x = ts_1 - ts_0
+                    if delta_x >= 0:
+                        delta = delta_x
+                    else:
+                        print "Warning! Queries arriving faster than TTL should allow"
+                        print self.domain, ts_1, ts_0, ts_1-ts_=, self.max_ttl
+                        result.append(-1)
+                        flag = True
+                        break
+                estimate += delta
+                count += 1
+            if flag:
+                continue
+            else:
+                result.append(count/float(estimate))
+        return result
+
+def estimate_day(tname):
+    domains = getDomains(tname)
+    domain_rates = {}
+    for domain in domains:
+        r = Record(tname, domain)
+        domain = r.domain
+        resolvers = r.resolvers
+        query_rate = r.estimate_rate()
+
+        if domain not in domain_rates:
+            domain_rates[domain] = []
+        for i in range(0, len(query_rate)):
+            domain_rates[domain].append((resolvers[i], query_rate[i]))
+
+    rates = domain_rates.items()
+    return rates
+
+
 def main():
     data_to_process = '20130901'
     dns_tname = 'dns_'+data_to_process
-    domains = getDomains(dns_tname)
-    for domain in domains:
-        print domain
-    print len(domains)
+    rates = estimate_day(dns_tname)
+    output = open('query_rate_by_day.data', 'w')
+    for domain, query_rate in rates:
+        output.write("%s\t%s\n" %(domain, query_rate))
+    output.close()
 
-if __name__ == '__main__':main()
+if __name__ == '__main__':
+    main()
