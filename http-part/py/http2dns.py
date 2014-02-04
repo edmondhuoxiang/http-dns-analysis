dc6f87faf358a18e8038f937ea5248bd5af9d0a7
Xiang Huo <huoxiang5659@gmail.com>
2014-02-04 16:28:12 -0600

    fixed bug in join statement'

--- http-part/py/http2dns.py
+++ http-part/py/http2dns.py
@@ -38,7 +38,6 @@ def getTimeWindowOfDay(date, tz):
     timezone = pytz.timezone(tz)
     begin = date + ' 0:0:0'
     end = date + ' 23:59:59'
-
     date_begin = datetime.strptime(begin, "%Y%m%d %H:%M:%S")
     date_end = datetime.strptime(end, "%Y%m%d %H:%M:%S")
     date_begin_localized = timezone.localize(date_begin, is_dst=None)
@@ -54,8 +53,9 @@ def http2dns(httpTable, dnsTable, tname):
     data_to_process = tname[-8:]
     tw = getTimeWindowOfDay(data_to_process, 'US/Eastern') #time window
 
-    try: 
-        cur.execute('SELECT DISTINCT HOST FROM %s WHERE ts>%s AND ts<%s;' % (httpTable, tw[0], tw[1]))
+    try:
+        cur.execute('DELETE FROM %s WHERE ts < %s AND ts > %s;' % (httpTable, tw[0], tw[1]))
+        cur.execute('SELECT DISTINCT HOST FROM %s ;' % httpTable )
     except pg.DatabaseError, e:
         Log.error('%s : %s' %(httpTable, e.pgerror))
         exit(1)
@@ -67,16 +67,17 @@ def http2dns(httpTable, dnsTable, tname):
         INSERT INTO %s 
         SELECT %s.id, %s.id, %s.ts, %s.ts, %s.host, %s.ttls, %s.orig_h, %s.resp_h, %s.orig_h, %s.resp_h 
         FROM %s LEFT JOIN %s
-        ON (%s.host = %s.query AND %s.ts > %s.ts AND %s.ts < (%s.ts+%s.ttls) AND %s.ts > %s AND %s.ts < %s)
+        ON (%s.host = %s.query AND %s.ts > %s.ts AND %s.ts < (%s.ts+%s.ttls))
         WHERE (%s.host = \'%s\' OR %s.host = \'%s\');
         '''
         domain_brief = ''
-        if domain.split('.') == 'www':
-            domain.brief = '.'.join(domain.split('.')[1:])
+        if domain.split('.')[0] == 'www':
+            domain_brief = '.'.join(domain.split('.')[1:])
         else:
             domain_brief = domain
         try:
-            cur.execute(command % (tname, dnsTable, httpTable, dnsTable, httpTable, httpTable, dnsTable, dnsTable, dnsTable, httpTable, httpTable, httpTable, dnsTable, httpTable, dnsTable, httpTable, dnsTable, httpTable, dnsTable, dnsTable, httpTable, tw[0], httpTable, tw[1], httpTable, domain, httpTable, domain_brief))
+            print command % (tname, dnsTable, httpTable, dnsTable, httpTable, httpTable, dnsTable, dnsTable, dnsTable, httpTable, httpTable, httpTable, dnsTable, httpTable, dnsTable, httpTable, dnsTable, httpTable, dnsTable, dnsTable, httpTable, domain, httpTable, domain_brief)
+            cur.execute(command % (tname, dnsTable, httpTable, dnsTable, httpTable, httpTable, dnsTable, dnsTable, dnsTable, httpTable, httpTable, httpTable, dnsTable, httpTable, dnsTable, httpTable, dnsTable, httpTable, dnsTable, dnsTable, httpTable, domain, httpTable, domain_brief))
         except pg.DatabaseError, e:
             Log.error('%s : %s' %(httpTable, e.pgerror))
             exit(1)
