6fe684f61afb3b4ae162efdfa0997782888ea2a5
Xiang Huo <huoxiang5659@gmail.com>
2014-02-18 09:36:20 -0600

    added the limitation of ts

--- dns-part/py/estimate_hit_rate.py
+++ dns-part/py/estimate_hit_rate.py
@@ -112,12 +112,12 @@ def getAllCircles(domain, resolver, dns_tname, http_tname):
     #pdb.set_trace()
     for query in dns_queries:
         count = 0
+        ts_1 = float(str(query['ts']))
+        ttl = float(str(query['ttls']))
         while index < len(http_requests):
             request = http_requests[index]
             index = index + 1
             ts_0 = float(str(request['ts']))
-            ts_1 = float(str(query['ts']))
-            ttl = float(str(query['ttls']))
             if ts_0 > ts_1 and ts_0 <= (ts_1+ttl):
             #if request['ts'] > query['ts'] and request['ts'] <= (query['ts']+query['ttls']):
                 count = count + 1
