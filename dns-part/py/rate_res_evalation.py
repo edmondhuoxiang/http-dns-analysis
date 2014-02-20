a6d3f17d751081d87e22606dc91a6b46d57f05c7
Xiang Huo <huoxiang5659@gmail.com>
2014-02-19 19:00:07 -0600

    import pytz

--- dns-part/py/rate_res_evalation.py
+++ dns-part/py/rate_res_evalation.py
@@ -77,7 +77,8 @@ def getDNSQuery(domain, tname):
         Log.error('%s : %s : %s' % (tname, domain, e))
         exit(1)
 
-    for i in range(0, len(queries)-1):
+    i = 0
+    while i < len(queries)-1:
         dist = queries[i+1]['ts'] - queries[i]['ts']
         if dist < 1:
             del(queries[i+1])
